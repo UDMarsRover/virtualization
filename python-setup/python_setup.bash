a7045b7dc52b293aa0cb85452e82a2752c267d60
Ryan Kuederle <ryankuederle@icloud.com>
2025-01-22 20:53:31 -0500

    Fixed python dep path

--- python-setup/python_setup.bash
+++ python-setup/python_setup.bash
@@ -18,6 +18,6 @@ source ~/.bashrc
 echo "Python virtual environment has been created and configured to activate in every terminal session."
 
 # Install packages from general_dependencies.txt
-pip install -r ~/virtualization/general_dependencies.txt
+pip install -r ~/virtualization/python-setup/general_dependencies.txt
 
 echo "General packages have been successfully installed."
